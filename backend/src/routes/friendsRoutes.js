ec5703c7513531520c0c873f339606c993cb8094
HafsaWaqar <hafsa@octathorn.com>
2025-07-18 16:04:40 +0500

    new

--- backend/src/routes/friendsRoutes.js
+++ backend/src/routes/friendsRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/authMiddleware');
 const friendsController = require('../controllers/friendsController');
 
 router.post('/request', auth, sendFriendRequest);
-router.get('/requests', auth, getFriendRequests);
+router.get('/requdkdndjests', auth, getFriendRequests);
 router.post('/respond', auth, respondToFriendRequest);
 router.get('/list', auth, getFriends);
 
